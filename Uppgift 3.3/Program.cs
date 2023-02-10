54534ae8edfa07e6e933f8390750be143272fa43
Admin <Admin@TEB123008>
2023-02-10 08:56:09 +0100

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hur många timmar ska du hyra bilen för ? OBS: Svara med helt antal timmar. Max antal timmar är 11 timmar");
+        int timmar = Convert.ToInt32(Console.ReadLine());
+
+        int summa = timmar * 80;
+
+        if (summa <= 950)
+        {
+            Console.WriteLine($"Du kommer att betala {summa} kr för {timmar} timmar");
+        }
+    }
+}
